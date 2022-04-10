c50ff69741eecd8ff26b2d6259b7871d6823db5a
Grzegorz Konopka <konopkagrzeg@gmail.com>
2022-04-10 21:20:01 +0200

    start implementing postfix

--- src/main/java/com/epam/jap/PostfixEvaluation.java
+++ src/main/java/com/epam/jap/PostfixEvaluation.java
@@ -1,5 +1,7 @@
 package com.epam.jap;
 
+import java.util.LinkedList;
+
 public class PostfixEvaluation implements Evaluation {
 
     private String value;
@@ -12,8 +14,26 @@ public class PostfixEvaluation implements Evaluation {
     @Override
     public int evaluate() {
 
-        for (int i = 0; i < value.length(); i++) {
+        LinkedList<Character> stack = new LinkedList<>();
+        int result = 0;
 
+        for (int i = 0; i < value.length(); i++) {
+            if (Character.isDigit(value.charAt(i)))
+                stack.push(value.charAt(i));
+            else {
+                if (Character.isDigit(value.charAt(i)))
+                    stack.push(value.charAt(i));
+                else {
+                    char operator = value.charAt(i);
+
+                    switch (operator) {
+                        case '+':
+                            int value1 = stack.pop();
+                            int value2 = stack.pop();
+                            result += value1 + value2;
+                    }
+                }
+            }
         }
         return 0;
     }
